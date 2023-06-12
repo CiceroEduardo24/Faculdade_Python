628936ef9a41fe2385a405f76a3d472b11d47a94
Cicero Eduardo <132598847+CiceroEduardo24@users.noreply.github.com>
2023-06-12 16:59:38 -0300

    err0
    err0

--- 03Orientado_a_objetos/01Agregação/Classe_Salario.py
+++
@@ -1,8 +0,0 @@
-#Class Salário
-class Salario:
-    def __init__(self, base, bonus):
-        self.base = base
-        self.bonus = bonus
-
-    def salario_anual(self):
-        return (self.base*12)+self.bonus
--- 03Orientado_a_objetos/05Exemplo_completo/todo-parte/Classe_Extrato.py
+++
@@ -1,7 +0,0 @@
-#Composição
-class Extrato:
-    def __init__(self):
-        self.transacoes = []
-    def imprimir(self):
-        for p in self.transacoes:
-            print(p[0], p[1])
